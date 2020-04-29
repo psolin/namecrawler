492128589515fbc61c3222a8b8c171b4c0d69aa9
Paul <paulsolin@gmail.com>
2020-04-28 20:07:39 -0600

    Update process.py
    Add unzip database function before some code execution.

--- namecrawler/process.py
+++ namecrawler/process.py
@@ -1,6 +1,35 @@
 from nameparser import HumanName
+import zipfile
+import os.path
 
 
-def fl_parse(name):
-    name = HumanName(name)
-    return(name.first, name.last)
+def database_unzip():
+    file_path = "data/names.sqlite"
+    if not os.path.isfile(file_path):
+        with zipfile.ZipFile(file_path + ".zip", 'r') as zip_ref:
+            zip_ref.extractall("data/")
+
+
+def name_parsing(name_str):
+    name_processed = HumanName(name_str)
+    return name_processed.first, name_processed.last
+
+
+def race(last_name):
+    database_unzip()
+    return last_name
+
+
+def age(first_name):
+    database_unzip()
+    return 0
+
+
+def sex(first_name):
+    database_unzip()
+    return 0
+
+
+def crawler(str):
+    database_unzip()
+    return 0
